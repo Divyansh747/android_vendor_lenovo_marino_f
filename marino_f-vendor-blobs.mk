fd2f0a21adf265a6123db1fde4988eec87e8a6c0
divyansh747 <divyansh.1999.dr@gmail.com>
2018-04-21 16:23:44 +0530

    update vendor

--- marino_f-vendor-blobs.mk
+++ marino_f-vendor-blobs.mk
@@ -5,6 +5,22 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/ven
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/lib64,system/vendor/lib64)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/app,system/vendor/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/framework,system/vendor/framework)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/media,system/vendor/media)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/plugin,system/vendor/plugin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/res,system/vendor/res)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/thh,system/vendor/thh)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/bin,system/bin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/lib,system/lib)
@@ -13,14 +29,10 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/lib
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/etc,system/vendor/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/firmware,system/vendor/firmware)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/usr,system/usr)
 
-#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/framework,system/framework)
 
-#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/xbin,system/vendor/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/priv-app,system/priv-app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/thh,system/vendor/thh)
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/thh,system/vendor/thh)
